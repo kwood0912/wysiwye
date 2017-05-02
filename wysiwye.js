4104f8c02f884043105de57f854f3b9dbbe44357
Kiel Wood <khwood0912@gmail.com>
2017-05-02 00:20:27 -0400

    stuff and things

---
+++ wysiwye.js
@@ -0,0 +1,4 @@
+var wysiwye = function () {
+	this.html = '<!DOCTYPE html><html><head><meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1" /></head><body></body></html>'
+	
+};
