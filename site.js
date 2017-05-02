203bfcb8b07ca4a64c135a160f50af5c87c585c4
Unknown <kiel.wood@massfinity.com>
2017-05-02 17:02:38 -0400

    got some stuff done

---
+++ site.js
@@ -0,0 +1,75 @@
+var bodyBackground, bodyTextColor, bodyLinkColor, bodyFontSize;
+var template = null;
+$(document).ready(function() {
+	//initialize gui variables
+	template = new wysiwye();
+	bodyBackground = $('#bodybg');
+	bodyTextColor = $('#bodytxtc');
+	bodyLinkColor = $('#bodyLinkColor');
+	bodyFontSize = $('#bodyFontSize');
+	body = $('#body');
+	contentContainer = $('#contentContainer');
+	ccBackground = $('#ccbg');
+	ccWidth = $('#ccWidth');
+	ccPadding = $('#ccPadding');
+
+	//set variables
+	$(body).css('background', '#' + $(bodyBackground).val());
+	$(body).css('color', '#' + $(bodyTextColor).val());
+	$('#body a').css('color', '#' + $(bodyLinkColor).val());
+	$(body).css('font-size', $(bodyFontSize).val() + 'px');
+	$(contentContainer).css('background', '#' + $(ccBackground).val());
+	$(contentContainer).css('max-width', $(ccWidth).val() + 'px');
+	$(contentContainer).css('padding', $(ccPadding).val() + 'px');
+
+	//defint event handlers
+	$('.ctrl-expando').click(function(){
+		var arrow = $(this).find('.caret');
+		var contentId = $(this).attr('expand-content');
+		if ($(arrow).hasClass('arrow-right')) {
+			//collapsed
+			$(arrow).removeClass('arrow-right').addClass('arrow-down');
+			$(contentId).show();
+			$(contentId).animate({
+				height: $(contentId).get(0).scrollHeight
+			}, 500, function () {
+				$(this).height('auto');
+			});
+		} else {
+			//expanded
+			$(arrow).removeClass('arrow-down').addClass('arrow-right');
+			$(contentId).animate({
+				height: 0
+			}, 500, function() {
+				$(this).hide();
+			});
+		}
+	});
+
+	$(bodyBackground).change(function() {
+		$(body).css('background', '#' + $(this).val());
+	});
+	$(bodyTextColor).change(function() {
+		$(body).css('color', '#' + $(this).val());
+	});
+	$(bodyLinkColor).change(function() {
+		$('#body a').css('color', '#' + $(this).val());
+	});
+	$(bodyFontSize).change(function() {
+		$(body).css('font-size', $(this).val() + 'px');
+	});
+	$(ccBackground).change(function() { 
+		$(contentContainer).css('background', '#' + $(this).val());
+	});
+	$(ccWidth).change(function() {
+		$(contentContainer).css('max-width', $(this).val() + 'px');
+	});
+	$(ccPadding).change(function() {
+		$(contentContainer).css('padding', $(this).val() + 'px');
+	});
+
+	$('.module-add').click(function() {
+		var module = $(this).attr('module');
+		template.addModule(module);
+	});
+});
--- wysiwye.js
+++ wysiwye.js
@@ -1,4 +1,58 @@
-var wysiwye = function () {
-	this.html = '<!DOCTYPE html><html><head><meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1" /></head><body></body></html>'
-	
+function wysiwye(preview) {
+	this.previewElement = preview;
+	this.globalDefaults = {
+		bodyBackground: 'FAFAFA',
+		bodyTextColor: 'FFFFFF',
+		bodyLinkColor: '1188e6',
+		contentContainerBackground: 'FFFFFF',
+		contentContainerWidth: '600',
+		contentContainerPadding: '8'
+	};
+	this.moduleDefaults = {
+		image: {
+			url: '',
+			backgroundColor: '#FFFFFF',
+			containerPadding: '0',
+			html: '<img style="max-width: 100%" src="" />'
+		},
+		text: {
+			text: 'Type something',
+			backgroundColor: 'FFFFFF',
+			containerPadding: '0',
+			html: '<div style="width:100%"></div>'
+		},
+		button: {
+			buttonColor: '1188E6',
+			borderColor: '1288E5',
+			fontColor: 'FFFFFF',
+			width: '200',
+			height: '15',
+			padding: '12',
+			borderRadius: '6',
+			fontSize: '16',
+			buttonText: 'Your Call-to-action',
+			buttonUrl: '',
+			alignment: 'center',
+			backgroundColor: 'FFFFFF',
+			containerPadding: '0'
+		}
+	};
+	this.modules = []; 
+	this.html = '<!DOCTYPE html><html><head><meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1" /></head><body></body></html>';
+	this.module = '<table><tr><td></td></tr></table>';
 };
+
+wysiwye.prototype.addModule = function(module) {
+	//push the default module onto the modules collection
+	this.modules.push(this.moduleDefaults[module]);
+	//add to the preview
+
+}
+
+wysiwye.prototype.removeModule = function(index) {
+	this.modules.splice(index, 1);
+}
+
+wysiwye.prototype.update = function() {
+	//go through and update the preview
+}
